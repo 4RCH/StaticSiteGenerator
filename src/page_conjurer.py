8c7294db60e452d672d06be38bd7562600a14224
4RCH <gfdaza@hotmail.co.uk>
2024-08-31 02:04:12 +0100

    - Added initial recursive copy - Added/updated project markdown pages

--- src/page_conjurer.py
+++ src/page_conjurer.py
@@ -1,5 +1,5 @@
 from block_markdown import markdown_to_blocks, markdown_to_html_node
-from copy_static import clean_up_folder
+from copy_static import clean_up_folder, get_path_tree, print_tree, make_copy
 import os
 
 def open_file(file_path):
@@ -46,3 +46,41 @@ def generate_page(from_path, template_path, dest_path):
     #write the file to disk
     write_file(dest_path, page_content)
     return True
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    print(f"\n[i] Conjuring pages...\n")
+    print(f"    from : {dir_path_content}\n    to : {dest_dir_path}")
+    try:
+        template_file = open_file(template_path)
+        if template_file:
+            print(f'    using template "{template_path}"\n')
+    except Exception as e:
+        print(f'[!] template {template_file} is missing')
+    
+    folder_structure = get_path_tree(dir_path_content)
+    print("[i] The source structure looks like this:\n")
+    print_tree(folder_structure)
+    
+    for file in folder_structure:
+        if os.path.isfile(file):
+            file = os.path.splitext(file)
+            if file[1] == 'md':
+                html_node = markdown_to_html_node(file)
+                html_content = html_node.to_html()
+                print(f'[i] Generated HTML content:\n{html_content}\n') # Can be safely deleted after tests
+                page_title = extract_title(file)
+                page_content = template_file.replace('{{ Title }}', page_title).replace('{{ Content }}', html_content)
+                write_file(dest_dir_path,page_content)
+            else:
+                print(f'{file} might not be a markdown file')
+
+    return True
+
+def main():
+    src = './tests/content'
+    des = './tests/public_test'
+    templates = 'template.html'
+    generate_pages_recursive(src, templates ,des)
+
+if __name__ == "__main__":
+    main()
